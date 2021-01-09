bba8911c2ed07d231443ffede0d63402b6439291
蔡聿善 <jimmy012403976@gmail.com>
2021-01-09 19:43:56 +0800

    first commit

---
+++ SerializationDataTranslator/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace SerializationDataTranslator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(426, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(475, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "開檔byte";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(29, 81);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(507, 319);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(573, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "<-------------->";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(542, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "byte";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(637, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "data";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(475, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "存檔byte";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 430);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(445, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "轉Data";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(676, 430);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(426, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "轉byte";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(29, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(426, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(676, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(426, 22);
+            this.textBox3.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(676, 43);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(426, 22);
+            this.textBox4.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1122, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "開檔data";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1122, 43);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "存檔data";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(676, 81);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(507, 319);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1211, 556);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "SerializationDataTranslator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ SerializationDataTranslator/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using UnityNetwork;
+using ScintillaNET;
+
+namespace SerializationDataTranslator
+{
+    public partial class Form1 : Form
+    {
+        Scintilla richTextBox2;
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox2 = NewScintilla("cs", "");
+            panel1.Controls.Add(richTextBox2);
+        }
+
+        private Scintilla NewScintilla(string Language, string text)
+        {
+            Scintilla Myediter;
+            Myediter = new Scintilla();
+            Myediter.Margins.Margin1.Width = 1;
+            Myediter.Margins.Margin0.Type = MarginType.Number;
+            Myediter.Margins.Margin0.Width = 0x23;
+            Myediter.ConfigurationManager.Language = Language;
+            Myediter.Dock = DockStyle.Fill;
+            Myediter.Scrolling.ScrollBars = ScrollBars.Both;
+            Myediter.ConfigurationManager.IsBuiltInEnabled = true;
+            Myediter.AppendText(text);
+            Myediter.UndoRedo.EmptyUndoBuffer();
+            return Myediter;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "txt檔|*.txt|所有檔案|*.*";
+            openFileDialog1.Title = "選擇你要開啟的檔案";
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != "")
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                try
+                {
+                    using (FileStream fileStream = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        byte[] a = new byte[fileStream.Length];
+                        fileStream.Read(a, 0, a.Length);
+                        int b = 0;
+                        string c = "";
+                        c = SerializationData.ReadString(a);
+                        richTextBox1.Text = c;
+                    }
+                }
+                catch (Exception ee)
+                {
+                    richTextBox1.Text = ee.ToString();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "txt檔|*.txt|所有檔案|*.*";
+            saveFileDialog1.Title = "選擇你要儲存的檔案";
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName != "")
+            {
+                try
+                {
+                    textBox2.Text = saveFileDialog1.FileName;
+                    byte[] q;
+                    q = SerializationData.WriteString(richTextBox1.Text);
+                    using (FileStream fileStream = new FileStream(saveFileDialog1.FileName, FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        fileStream.Write(q, 0, q.Length);
+                    }
+                }
+                catch (Exception ee)
+                {
+                    richTextBox1.Text = ee.ToString();
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "txt檔|*.txt|所有檔案|*.*";
+            openFileDialog1.Title = "選擇你要開啟的檔案";
+            openFileDialog1.FileName = "";
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != "")
+            {
+                textBox3.Text = openFileDialog1.FileName;
+                richTextBox2.Text = File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "txt檔|*.txt|所有檔案|*.*";
+            saveFileDialog1.Title = "選擇你要儲存的檔案";
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName != "")
+            {
+                textBox4.Text = saveFileDialog1.FileName;
+                File.WriteAllText(saveFileDialog1.FileName, richTextBox2.Text);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] vs;
+                vs = SerializationData.WriteString(richTextBox1.Text);
+                richTextBox2.Text = SerializationData.BytesToStringWithEnter(vs);
+            }
+            catch (Exception ee)
+            {
+                richTextBox2.Text = ee.ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = SerializationData.ReadString(SerializationData.StringToBytes(richTextBox2.Text));
+            }
+            catch (Exception ee)
+            {
+                richTextBox1.Text = ee.ToString();
+            }
+        }
+    }
+}
